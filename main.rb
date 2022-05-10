7597f2ed97fff31533988ce67ecad259f39e3f7d
Baleria Reyes <breyes815@gmail.com>
2022-05-10 08:58:12 -0700

    extraction of classes, Button, and Window.

--- main.rb
+++ main.rb
@@ -11,30 +11,10 @@
 
 class Window
 
-    def initialize(foreground_color = 0, other_color = 0)
-        @foreground_color
-        @other_color
-    end
 
-    def paint(is_dark_mode)
-        if is_dark_mode
-            self.foreground_color = foreground_color - 10
-            self.other_color = '#111111'
-        else
-            self.foreground_color = foreground_color + 10
-            self.other_color = '#E0E0E0'
-
-    end
 end
 
 class Button
 
-    def initialize(x, y, label = "None")
-        @x_position = x
-        @y_position = y
-        @label
-    end
-    def label_text(label)
-        self.label = label
-    end
+
 end
